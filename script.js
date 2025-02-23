0a7b9285ba6775f9c49e082ced8f99bb61a3b6d8
EncinasEmwin <encinasemwin17@gmail.com>
2025-02-23 22:45:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let number = parseInt(prompt("Enter a number:"));
+
+if (number % 2 === 0) {
+    alert(number + " is even.");
+} else {
+    alert(number + " is odd.");
+}
+
+console.log("Numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
